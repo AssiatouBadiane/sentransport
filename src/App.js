0fff8094901d1f41071d9ecb35abbaf143c205bc
Assiatou Badiane <assiatoubadiane@esp.sn>
2026-05-01 03:25:50 +0000

    Ajout Footer, date Header et composant Statistique

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
 import Tableau from './components/Tableau';
+import Statistique from './components/Statistique';
 
 function App() {
-  const utilisateurs = [
-    {nom:"Badiane" },{nom:"Diallo" }
-  ]
   return (
     <div className="App">
       <Header />
       <main className="contenu">
         <p>Bienvenue ! Cette application vous aide à trouver votre ligne de bus à Dakar.</p>
-        <Tableau></Tableau>
+        <div className="stats-container">
+          <Statistique chiffre="10" libelle="Lignes" />
+          <Statistique chiffre="150" libelle="Arrêts" />
+          <Statistique chiffre="50" libelle="Bus" />
+        </div>
+        <Tableau />
       </main>
+      <Footer />
     </div>
   );
 }
